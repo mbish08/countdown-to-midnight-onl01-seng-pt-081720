707389fbe9c5343c2176bbc571689ec4aaeaf7ad
Mikel Bishop <mdrew8@gmail.com>
2020-08-23 16:52:28 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+n = 10 
+def countdown(n)
+  while n > 0 
+  puts "#{n} SECOND(S)!"
+  n -= 1
+end
+return "HAPPY NEW YEAR!"
+end
 
-def countdown
+n = 10 
+def countdown_with_sleep(n)
+  while n > 0 
+  puts "#{n} SECOND(S)!"
+  n -= 1
+  sleep(1) 
+end
+return "HAPPY NEW YEAR!"
 end
